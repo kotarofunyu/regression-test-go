4f3d4c6ecefd1c2a96897c650e4d7d9fd3bc69d7
kotarofunyu <kotarofunyu@gmail.com>
2023-03-11 22:50:01 +0900

    パッケージ分割を反映

--- main.go
+++ main.go
@@ -1,22 +1,18 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
 	_ "image/jpeg"
-	"image/png"
 	"log"
 	"os"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/kotarofunyu/regression-test-go/gitcomparison"
+	"github.com/kotarofunyu/regression-test-go/urlcomparison"
 
-	diff "github.com/olegfedoseev/image-diff"
 	agouti "github.com/sclevine/agouti"
 )
 
@@ -24,116 +20,9 @@ type Comparer interface {
 	Run()
 }
 
-type GitComparison struct {
-	repository   *git.Worktree
-	beforebranch string
-	afterbranch  string
-	baseurl      string
-	paths        []string
-	breakpoints  []int
-	page         *agouti.Page
-}
-
-type UrlComparison struct {
-	beforebaseurl string
-	afterbaseurl  string
-	paths         []string
-	breakpoints   []int
-	page          *agouti.Page
-}
-
-func (gc *GitComparison) Run() {
-	os.Mkdir("results/", os.ModePerm)
-	os.Mkdir("captures/", os.ModePerm)
-	var wg sync.WaitGroup
-	var mu sync.Mutex
-	for _, path := range gc.paths {
-		wg.Add(1)
-		go func(wg *sync.WaitGroup, path string) {
-			// NOTE: goroutine間でagouti.Pageを共有するので排他制御が必要
-			mu.Lock()
-			defer wg.Done()
-			defer mu.Unlock()
-			gc.page.Navigate(gc.baseurl + path)
-			for _, breakpoint := range gc.breakpoints {
-				before := "./captures/before-" + path + "-" + strconv.Itoa(breakpoint) + ".png"
-				after := "./captures/after-" + path + "-" + strconv.Itoa(breakpoint) + ".png"
-				os.Create(before)
-				os.Create(after)
-				var height int
-				if err := gc.page.RunScript("return document.body.scrollHeight;", nil, &height); err != nil {
-					log.Fatal(err)
-				}
-				gc.page.Size(breakpoint, height)
-				if err := checkoutGitBranch(gc.repository, gc.beforebranch); err != nil {
-					log.Fatal(err)
-				}
-				gc.page.Refresh()
-				gc.page.Screenshot(before)
-				if err := checkoutGitBranch(gc.repository, gc.afterbranch); err != nil {
-					log.Fatal(err)
-				}
-				gc.page.Refresh()
-				gc.page.Screenshot(after)
-				compareFiles(before, after, path, breakpoint)
-			}
-		}(&wg, path)
-	}
-	wg.Wait()
-	// NOTE: ファイルへの書き込みをやめてバイナリをメモリに保持して比較する方が省エネかも
-	// defer rt.cleanupCaptures(before, after)
-}
-
-func (uc *UrlComparison) Run() {
-	os.Mkdir("results/", os.ModePerm)
-	os.Mkdir("captures/", os.ModePerm)
-	for _, path := range uc.paths {
-		for _, breakpoint := range uc.breakpoints {
-			before := "./captures/before-" + path + "-" + strconv.Itoa(breakpoint) + ".png"
-			os.Create(before)
-			after := "./captures/after-" + path + "-" + strconv.Itoa(breakpoint) + ".png"
-			os.Create(after)
-			uc.page.Navigate(uc.beforebaseurl + path)
-			var height int
-			if err := uc.page.RunScript("return document.body.scrollHeight;", nil, &height); err != nil {
-				log.Fatal(err)
-			}
-			uc.page.Size(breakpoint, height)
-			uc.page.Screenshot(before)
-			uc.page.Navigate(uc.afterbaseurl + path)
-			uc.page.Size(breakpoint, height)
-			uc.page.Screenshot(after)
-			compareFiles(before, after, path, breakpoint)
-		}
-	}
-}
-
 func cleanupCaptures(before, after string) {
 	os.Remove(before)
 	os.Remove(after)
-}
-
-func compareFiles(before, after, path string, breakpoint int) {
-	diff, percent, err := diff.CompareFiles(before, after)
-	if err != nil {
-		log.Fatal(err, before, after)
-	}
-	if percent == 0.0 {
-		fmt.Println("Image is same!")
-		return
-	}
-	t := time.Now()
-	ft := t.Format("20200101123045")
-	diffName := "diff-" + path + "-" + strconv.Itoa(breakpoint) + "px" + "-" + ft + ".png"
-	destDir := "./results/"
-	f, err := os.Create(destDir + diffName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	buf := new(bytes.Buffer)
-	png.Encode(buf, diff)
-	f.Write(buf.Bytes())
-	fmt.Println("diff has written into " + destDir + diffName)
 }
 
 func setupBrowser() (*agouti.Page, *agouti.WebDriver) {
@@ -175,52 +64,18 @@ func setupArgs() (baseUrl string, paths []string, breakpoints []int, gitpath, be
 	return
 }
 
-func checkoutGitBranch(wt *git.Worktree, destbranch string) error {
-	err := wt.Checkout(
-		&git.CheckoutOptions{
-			Branch: plumbing.NewBranchReferenceName(destbranch),
-		},
-	)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
 func main() {
 	now := time.Now()
 	baseUrl, paths, breakpoints, gitpath, beforebranch, afterbranch, beforeurl, afterurl := setupArgs()
 	page, driver := setupBrowser()
 	defer driver.Stop()
 
+	var comparer Comparer
 	if len(gitpath) > 0 {
-		r, err := git.PlainOpen(gitpath)
-		if err != nil {
-			log.Fatal(err)
-		}
-		wt, err := r.Worktree()
-		if err != nil {
-			log.Fatal(err)
-		}
-		gc := GitComparison{
-			repository:   wt,
-			beforebranch: beforebranch,
-			afterbranch:  afterbranch,
-			baseurl:      baseUrl,
-			paths:        paths,
-			breakpoints:  breakpoints,
-			page:         page,
-		}
-		gc.Run()
+		comparer = gitcomparison.NewGitComparison(gitpath, beforebranch, afterbranch, baseUrl, paths, breakpoints, page)
 	} else {
-		uc := UrlComparison{
-			beforebaseurl: beforeurl,
-			afterbaseurl:  afterurl,
-			paths:         paths,
-			breakpoints:   breakpoints,
-			page:          page,
-		}
-		uc.Run()
+		comparer = urlcomparison.NewUrlComparison(beforeurl, afterurl, paths, breakpoints, page)
 	}
+	comparer.Run()
 	fmt.Printf("Completed in: %vms\n", time.Since(now).Milliseconds())
 }
