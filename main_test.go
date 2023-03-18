90269cf855bd5d7679182c6fce6577c36e80a874
kotarofunyu <kotarofunyu@gmail.com>
2023-03-18 12:12:06 +0900

    テストを修正

--- main_test.go
+++ main_test.go
@@ -1,26 +1,57 @@
 package main
 
 import (
+	"io/fs"
 	"io/ioutil"
 	"testing"
 )
 
 func Test_compareFiles(t *testing.T) {
-	before := "./testdata/sample.png"
-	after := "./testdata/sample_copy.png"
-	path := "./testdata"
-	breakpoint := 768
-	compareFiles(before, after, path, breakpoint)
-	all, _ := ioutil.ReadDir("./")
-	var exits bool
-	for _, e := range all {
-		if e.Name() == "results" {
-			exits = true
-			return
-		}
+	cases := []struct {
+		before     string
+		after      string
+		path       string
+		breakpoint int
+		diff       bool
+	}{
+		{
+			before:     "./testdata/sample.png",
+			after:      "./testdata/sample_copy.png",
+			path:       "sample",
+			breakpoint: 768,
+			diff:       false,
+		},
+		{
+			before:     "./testdata/sample.png",
+			after:      "./testdata/sample_diff.png",
+			path:       "sample",
+			breakpoint: 768,
+			diff:       true,
+		},
 	}
 
-	if exits != true {
-		t.Errorf("error: %#v", exits)
+	for _, tt := range cases {
+		beforelen := countFiles("./results")
+		compareFiles(tt.before, tt.after, tt.path, tt.breakpoint)
+		afterlen := countFiles(("./results"))
+
+		if tt.diff {
+			if beforelen == afterlen {
+				t.Error(beforelen, afterlen)
+			}
+		} else {
+			if beforelen != afterlen {
+				t.Error(beforelen, afterlen)
+			}
+		}
 	}
+}
+
+func countFiles(dirpath string) int {
+	files, _ := ioutil.ReadDir(dirpath)
+	var filesInfos []fs.FileInfo
+	for _, fi := range files {
+		filesInfos = append(filesInfos, fi)
+	}
+	return len(filesInfos)
 }
